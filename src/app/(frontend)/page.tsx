97582ab0e5cedd193477cc98b53bf6a268f83106
Jose <jose.gr98@yahoo.com>
2025-03-18 17:29:24 -0300

    -disabled pre rendering

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { RenderUi } from '@/components/RenderUi/RenderUi'
 import { getPage } from '@/network/getPage'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function HomePage() {
   const page = await getPage({ path: '/' })
 
