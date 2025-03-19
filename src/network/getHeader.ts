2736e4ae1996d6cd3df654b614a4266e216d7c79
Jose <jose.gr98@yahoo.com>
2025-03-19 16:29:15 -0300

    -Fixed header rendering

--- src/network/getHeader.ts
+++ src/network/getHeader.ts
@@ -1,10 +1,11 @@
 import { Header } from '@/payload-types'
 import { getCacheConfig } from './getCacheConfig'
+import { PaginatedDocs } from 'payload'
 
 const getUrl = () =>
   process.env.IS_PREVIEW === 'true'
-    ? `${process.env.BASE_URL}/api/header/1?depth=3&draft=true&locale=undefined`
-    : `${process.env.BASE_URL}/api/header/1?depth=3&draft=false&locale=undefined`
+    ? `${process.env.BASE_URL}/api/header?depth=3&draft=true&locale=undefined`
+    : `${process.env.BASE_URL}/api/header?depth=3&draft=false&locale=undefined`
 
 export const getHeader = async (): Promise<Header> => {
   const cacheConfig = getCacheConfig()
@@ -14,7 +15,7 @@ export const getHeader = async (): Promise<Header> => {
     ...cacheConfig,
   })
 
-  const data = await result.json()
+  const data = (await result.json()) as PaginatedDocs<Header>
 
-  return data as Header
+  return data.docs[0]
 }
